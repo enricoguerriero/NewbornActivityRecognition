a7784ec6d37277adb39602c46bb665c5b2d3668b
enricoguerriero <enrico.guerriero@gmail.com>
2025-04-08 09:28:43 +0200

    a

--- engines/janus7B.py
+++ engines/janus7B.py
@@ -1,4 +1,5 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoModelForCausalLM
+from janus.models import MultiModalityCausalLM, VLChatProcessor
 import torch
 from PIL import Image
 import re
@@ -13,9 +14,11 @@ class JanusProEngine:
         self.device = torch.device(device if device else ('cuda' if torch.cuda.is_available() else 'cpu'))
         
         # Load the processor and model
-        self.processor = AutoTokenizer.from_pretrained(model_id)
+        self.vl_chat_processor = VLChatProcessor.from_pretrained(model_id)
+        self.tokenizer = self.vl_chat_processor.tokenizer
         self.model = AutoModelForCausalLM.from_pretrained(
-            model_id,
+            model_id, 
+            trust_remote_code=True,
             torch_dtype=torch.float16,
             device_map="auto"
         ).to(self.device)
@@ -25,12 +28,20 @@ class JanusProEngine:
         
         self.name = "janus_pro"
 
-    def prompt_definition(self, question: str):
+    def prompt_definition(self, question: str, image: Image):
         """
         Define the prompt structure for the model.
         """
-        prompt_template = f"Answer the following question based on the provided image: {question}"
-        return prompt_template
+        conversation = [
+            {
+                "role": "<|User|>",
+                "content": f"<image_placeholder>\n {question}",
+                "images": [image],
+            },
+            {"role": "<|Assistant|>", "content": ""},
+        ]
+
+        return conversation
 
     def answer_question(self, image_list: list, question: str, seed: int = 42, temperature: float = 0.1):
         """
@@ -42,13 +53,11 @@ class JanusProEngine:
         image = Image.fromarray(image_list[len(image_list) // 2])
         
         # Define the prompt for the current question
-        # prompt_text = self.prompt_definition(question)
-        prompt_text = question
+        prompt_text = self.prompt_definition(question, image)
         
         # Process inputs (both text and image)
-        inputs = self.processor(
-            text=prompt_text,
-            images=image,
+        inputs = self.vl_chat_processor(
+            conversation=prompt_text,
             return_tensors="pt"
         ).to(self.device)
         
