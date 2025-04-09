1deb2824b3af20d17ecd0f8dc67646b472f194b9
enricoguerriero <enrico.guerriero@gmail.com>
2025-04-09 14:52:11 +0200

    tf

--- data/utils.py
+++ data/utils.py
@@ -2,20 +2,20 @@ from PIL import Image
 
 class LeftCrop:
     def __init__(self, size):
-        self.size = size  # assumes size is an int (for square crop)
+        self.size = size  
 
     def __call__(self, img: Image.Image):
         width, height = img.size
-        crop_size = self.size
+        crop_width, crop_height = self.size
 
         # Ensure image is large enough
-        if width < crop_size or height < crop_size:
-            raise ValueError(f"Image size {img.size} is smaller than crop size {crop_size}")
+        if width < crop_width or height < crop_height:
+            raise ValueError(f"Image size {img.size} is smaller than crop size {self.size}")
 
         # Calculate left crop coordinates
         left = 0
-        upper = (height - crop_size) // 2
-        right = crop_size
-        lower = upper + crop_size
+        upper = (height - crop_height) // 2
+        right = crop_width
+        lower = upper + crop_height
 
         return img.crop((left, upper, right, lower))
