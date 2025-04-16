4d388d7ce34592d7c8f395259ea51ac82b2fe152
enricoguerriero <enrico.guerriero@gmail.com>
2025-04-16 16:17:06 +0200

    tf

---
+++ configs/llavavideo_classifier.py
@@ -0,0 +1,11 @@
+from .base_config import BASE_CONFIG
+
+CONFIG = BASE_CONFIG.copy()
+
+CONFIG.update({
+    "clip_length": 2,
+    "frames_per_second": 6,
+    "overlap": 1,
+    
+    "output_dir": "models/saved/llavavideo_classifier",
+})
