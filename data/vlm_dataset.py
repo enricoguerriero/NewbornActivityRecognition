a3a8af3b6c9b8ef7891dbf57b93723c191a85540
enricoguerriero <enrico.guerriero@gmail.com>
2025-04-18 17:29:32 +0200

    db

--- data/vlm_dataset.py
+++ data/vlm_dataset.py
@@ -22,7 +22,7 @@ class ClipDataset(Dataset):
         labels = video_data['labels']
        
         inputs = self.processor(text=self.prompt, images=frames, return_tensors="pt", padding=True)
-        
+        print(inputs.keys(), flush = True)
         return {
             "input_ids": inputs["input_ids"].squeeze(0),
             "attention_mask": inputs["attention_mask"].squeeze(0),
